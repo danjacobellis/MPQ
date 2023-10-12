7d33af3710ba8d965dbcf4941da09f7201175e40
Dan Jacobellis <danjacobellis@utexas.edu>
2023-10-12 10:17:38 -0500

    audio

--- utils.py
+++ utils.py
@@ -4,6 +4,9 @@ import tempfile
 import compressai
 import torch
 import torch.nn.functional as F
+import torchaudio
+import opuspy
+import encodec
 from io import BytesIO
 from torchvision import transforms
 try:
@@ -87,4 +90,47 @@ def hific_lo_compress(sample):
                 
     sample['image'] = output_img
     sample['bpp'] = bpp
+    return sample
+
+def mp3_compress(sample):
+    audio = sample['audio']['array'].unsqueeze(0)
+    fs = sample['audio']['sampling_rate']
+    with BytesIO() as f:
+        torchaudio.save(f, audio, sample_rate=fs, format="mp3", compression=8.0)
+        f.seek(0)
+        sample['bps'] = len(f.getvalue())/len(audio[0])
+        audio = torchaudio.load(f,format="mp3")
+    sample['audio']['array'] = audio[0][0]
+    return sample
+
+def opus_compress(sample):
+    audio = sample['audio']['array'].unsqueeze(1)
+    audio = (2**15*audio.clamp(min=-1.0,max=1.0)).to(torch.int16).numpy()
+    fs = sample['audio']['sampling_rate']
+    with tempfile.NamedTemporaryFile('wb', delete=True) as f:
+        opuspy.write(
+            path=f.name,
+            waveform_tc=audio,
+            sample_rate=fs,
+            bitrate=6000,
+            signal_type=2,
+            encoder_complexity=10
+        )
+        audio,fs = opuspy.read(f.name)
+        audio = torch.tensor(audio).to(torch.float32)/(2**15)
+        audio = audio.transpose(0,1)
+        sample['bps'] = os.path.getsize(f.name)/len(audio[0])
+    sample['audio']['array'] = audio[0]
+    return sample
+
+
+def encodec_compress(sample,model,device):
+    audio = sample['audio']['array'].unsqueeze(0)
+    fs = sample['audio']['sampling_rate']
+    audio = encodec.utils.convert_audio(audio,fs,model.sample_rate,model.channels)
+    with torch.no_grad():
+        x = audio.unsqueeze(0).to(device)
+        encoded_frames = model.encode(x)
+        sample['audio']['array'] = model.decode(encoded_frames).mean(dim=[0,1])
+        sample['bps'] = (40/8)*sum(fi[0].shape[2] for fi in encoded_frames)/sample['audio']['array'].shape[0]
     return sample
